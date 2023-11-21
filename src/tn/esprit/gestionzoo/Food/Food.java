6c4dca13c0b986840791c32b4156d6d529c1d704
Azza0411 <hamdi.azza@esprit.tn>
2023-11-21 21:17:39 +0100

    ajout prosit9

--- src/tn/esprit/gestionzoo/Food/Food.java
+++
@@ -1,8 +0,0 @@
-package tn.esprit.gestionzoo.Food;
-
-// Instruction 36 :---------------------------------------------
-public enum Food
-{
-    MEAT, PLANT,BOTH
-}
-//-----------------------------------------------------------------
